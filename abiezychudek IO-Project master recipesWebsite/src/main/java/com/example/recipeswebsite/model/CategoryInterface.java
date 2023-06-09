ea575124e5dbd0c8f7c830fd56f8907e9316f4a3
Alicja Bieżychudek <94805433+abiezychudek@users.noreply.github.com>
2023-06-09 02:28:48 +0200

    Add files via upload

---
+++ abiezychudek IO-Project master recipesWebsite/src/main/java/com/example/recipeswebsite/model/CategoryInterface.java
@@ -0,0 +1,14 @@
+package com.example.recipeswebsite.model;
+
+public interface CategoryInterface {
+    public enum Categories {
+        POTATO,
+        TOMATO,
+        CUCUMBER,
+        ONION,
+        CARROT,
+        CABBAGE,
+        SALT,
+        PEPPER
+    }
+}
