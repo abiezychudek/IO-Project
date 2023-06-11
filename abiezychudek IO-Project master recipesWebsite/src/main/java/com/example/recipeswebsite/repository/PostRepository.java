ebb3c5468920da76606e15e1d2564758775567c0
Aleksandra Grygorczyk <aleksandra.grygorczyk@gmail.com>
2023-06-11 13:05:48 +0200

    Deleted unnecessary folders

--- abiezychudek IO-Project master recipesWebsite/src/main/java/com/example/recipeswebsite/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.recipeswebsite.repository;
-
-import com.example.recipeswebsite.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
