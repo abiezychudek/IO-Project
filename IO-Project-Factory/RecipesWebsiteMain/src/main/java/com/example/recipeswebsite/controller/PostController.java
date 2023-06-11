06facee958eafa797dbadc4fd14a827edfa98630
Ignacy Kolton <96392714+ignajlo@users.noreply.github.com>
2023-06-11 23:51:33 +0200

    Update PostController.java
    rating import

--- IO-Project-Factory/RecipesWebsiteMain/src/main/java/com/example/recipeswebsite/controller/PostController.java
+++ IO-Project-Factory/RecipesWebsiteMain/src/main/java/com/example/recipeswebsite/controller/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
