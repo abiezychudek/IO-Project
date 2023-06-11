eae09e1f94185f14c38f925c65a77944e7eede2a
whykciu <122232469+whykciu@users.noreply.github.com>
2023-06-11 17:22:24 +0200

    Add files via upload

--- RecipesWebsiteMain/src/main/java/com/example/recipeswebsite/services/PostService.java
+++ RecipesWebsiteMain/src/main/java/com/example/recipeswebsite/services/PostService.java
@@ -1,11 +1,13 @@
 package com.example.recipeswebsite.services;
 
+import com.example.recipeswebsite.model.Comment;
 import com.example.recipeswebsite.model.Post;
 import com.example.recipeswebsite.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -45,6 +47,18 @@ public class PostService {
 
     public void delete (Post post) {
         postRepository.delete(post);
+    }
+
+    public void addCommentToPost(Long id, Comment c){
+
+        Optional<Post> post = postRepository.findById(id);
+        Post p = post.get();
+        c.setPostId(p);
+        c.setCreatedAt(new Date());
+        c.setAuthor("User");
+        p.getComments().add(c);
+        postRepository.save(p);
+
     }
 
 }
